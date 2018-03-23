7108dda257c325ced751f2c3ef4fd7b6346e7231
ellen <13112060@bjtu.edu.cn>
2018-03-23 17:16:43 +0800

    如果save文件夹存在，删除save文件夹及文件夹下所有内容 create save文件夹下所需文件夹

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from FaceAging import FaceAging
 import tensorflow as tf
+import shutil
+import os
 
 flags = tf.app.flags
 flags.DEFINE_integer(flag_name='epoch', default_value=200, docstring='number of epochs')
-flags.DEFINE_integer(flag_name='size_image', default_value=5, docstring='size of an image')
-flags.DEFINE_string(flag_name='size_batch', default_value=10, docstring='size of one batch')
+flags.DEFINE_integer(flag_name='size_image', default_value=64, docstring='size of an image')
+flags.DEFINE_string(flag_name='size_batch', default_value=128, docstring='size of one batch')
+flags.DEFINE_string(flag_name='size_name', default_value=140, docstring='size of name array, 133 or 1')
+flags.DEFINE_string(flag_name='size_name_total', default_value=140, docstring='size of total name')
 flags.DEFINE_boolean(flag_name='is_train', default_value=True, docstring='training mode')
 flags.DEFINE_string(flag_name='path_data', default_value='./data', docstring='upper dir of dataset')
-flags.DEFINE_string(flag_name='dataset_name', default_value='UTKFace', docstring='dataset name')
+flags.DEFINE_string(flag_name='dataset_name', default_value='CACD', docstring='dataset name')
 flags.DEFINE_string(flag_name='savedir', default_value='save', docstring='dir for saving training results')
 flags.DEFINE_string(flag_name='testdir', default_value='None', docstring='dir for testing images')
 flags.DEFINE_string(flag_name='image_mode', default_value='RGB', docstring='input image mode')
@@ -17,11 +21,24 @@ FLAGS = flags.FLAGS
 
 
 if __name__ == '__main__':
+
+    # delete save dir
+    import os
+    if os.path.exists(FLAGS.savedir):
+        shutil.rmtree(FLAGS.savedir)
+    # create save dir and it's baby dirs
+    os.makedirs(FLAGS.savedir + '/weight')
+    os.makedirs(FLAGS.savedir + '/image')
+    os.makedirs(FLAGS.savedir + '/metric')
+
+
     fa_model = FaceAging(
         is_training=True,  # flag for training or testing mode
         save_dir=FLAGS.savedir,  # path to save checkpoints, samples, and summary
         dataset_name=FLAGS.dataset_name,  # name of the dataset in the folder ./data
-        size_image=FLAGS.size_image
+        size_image=FLAGS.size_image,
+        size_name=FLAGS.size_name,
+        size_name_total=FLAGS.size_name_total
     )
     if FLAGS.is_train:
         print('\n\tTraining Mode')
